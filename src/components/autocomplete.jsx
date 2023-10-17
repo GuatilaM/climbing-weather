c793611a4c736d359e5e6ebcd097abd45a9746af
guatilaM <guatilam@gmail.com>
2023-10-17 18:29:49 -0500

    Autocomplete onChange working. Need to fix memory effect when clearing input

--- src/components/autocomplete.jsx
+++ src/components/autocomplete.jsx
@@ -1,77 +1,121 @@
-import {useState} from "react";
+import { useCallback, useEffect, useState } from "react";
 import './autocomplete.css';
 
-function Autocomplete(){
+// Test fetch code
+// async function getEggs() {
+//     try {
+//         const response = await fetch('https://reqres.in/api/users');
+//         const eggs = await response.json();
+//         console.log(eggs);
+//     } catch (error) {
+//         console.error();
+//     }
+// }
+// getEggs();
+
+function Autocomplete() {
 
     const [state, setState] = useState({
         activeSuggestion: 0,
         filteredSuggestions: [],
         showSuggestions: false,
-        userInput: ''  
+        userInput: '',
+        debouncedInput: ''
     });
 
-    // const handleChange = (event) => {
-    //     const uInput = event.target.value;
-    //     debounce((uInput) => getCities(uInput));
-    //     setState({
-    //         ...state,
-    //         userInput: uInput
-    //     });
-    //     console.log(uInput);
-    // } 
-    // function updateInputValue(event){
-    //     setState({
-    //         ...state,
-    //         userInput: event.target.value
-    //     });
-    //     getCities(event);
-    // }
-
-
-    const handleChange = debounce((event) => getCities(event));
-    // const handleChange = debounce((event) => updateInputValue(event));
-    // function handleChange(event) {
-    //     debounce(console.log(event));
-    //     setState({
-    //         ...state,
-    //         userInput: event.target.value
-    //     });
-    // }
     // const suggestions = ['Bogota', 'Cali', 'Buenaventura', 'Medellin', 'Leticia', 'Suesca'];
 
-    function debounce(funct, debounceTime = 600){
+    // const handleChange = debounce((event) => getCities(event));
+
+    function updateUserInput(event) {
+        setState({
+            ...state,
+            userInput: event.target.value
+        });
+    }
+
+    // Debounce user input change
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setState({
+                ...state,
+                debouncedInput: state.userInput
+            });
+        }, 600);
+        return () => clearTimeout(timeout);
+    }, [state, state.userInput]);
+
+    const getCities = useCallback(async () => {
+        const cityInput = state.debouncedInput;
+        if (cityInput === ''){return;}
+
+        const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${cityInput}&sort=-population&minPopulation=1000`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'b41b6ed316msh1f1ef6714b5d239p14e412jsn027b91fe2679',
+                'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await fetch(url, options);
+            if (!response.ok){
+                throw new Error('Too many calls to API');
+            }
+            const suggestions = await response.json();
+            console.log(suggestions);
+            const names = suggestions.data.map((cityInfo) => cityInfo.name);
+            //   updateSuggestions(cityInput, names);
+            setState({
+                ...state,
+                activeSuggestion: 0,
+                filteredSuggestions: names,
+                showSuggestions: true
+            });
+        } catch (error) {
+            console.error('Something went wrong...', error);
+        }
+    }, [state]);
+
+    // Make calls to GeoDB API on debounced input change
+    useEffect(() => {
+        getCities();
+    }, [state.debouncedInput]);
+
+    function debounce(funct, debounceTime = 600) {
         let timer;
         return (...args) => {
             clearTimeout(timer);
             timer = setTimeout(() => funct.apply(this, args), debounceTime);
-        }; 
-    }
-
-    async function getCities(event) {
-        const cityInput = event.target.value;
-        // const cityInput = event;
-        // console.log(cityInput);
-        const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${cityInput}&sort=-population&minPopulation=1000`;
-        const options = {
-          method: 'GET',
-          headers: {
-            'X-RapidAPI-Key': 'b41b6ed316msh1f1ef6714b5d239p14e412jsn027b91fe2679',
-            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-          }
         };
-    
-        try {
-          const response = await fetch(url, options);
-          const suggestions = await response.json();
-          console.log(suggestions);
-          const names = suggestions.data.map((cityInfo) => cityInfo.name);
-          updateSuggestions(cityInput, names);
-        } catch (error) {
-          console.error(error);
-        }
     }
 
-    function updateSuggestions(input, suggestions){
+    // async function getCities(event) {
+    //     const cityInput = event.target.value;
+    //     // const cityInput = event;
+    //     // console.log(cityInput);
+    //     const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${cityInput}&sort=-population&minPopulation=1000`;
+    //     const options = {
+    //         method: 'GET',
+    //         headers: {
+    //             'X-RapidAPI-Key': 'b41b6ed316msh1f1ef6714b5d239p14e412jsn027b91fe2679',
+    //             'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    //         }
+    //     };
+
+    //     try {
+    //         const response = await fetch(url, options);
+    //         const suggestions = await response.json();
+    //         console.log(suggestions);
+    //         const names = suggestions.data.map((cityInfo) => cityInfo.name);
+    //         updateSuggestions(cityInput, names);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }
+
+    function updateSuggestions(input, suggestions) {
         // const filtered = suggestions.filter(
         //     suggestion => suggestion.toLowerCase().indexOf(input.toLowerCase()) > -1
         // );
@@ -83,7 +127,7 @@ function Autocomplete(){
         })
     }
 
-    function updateInput(event){
+    function updateInput(event) {
         setState({
             activeSuggestion: 0,
             filteredSuggestions: [],
@@ -92,9 +136,9 @@ function Autocomplete(){
         });
     }
 
-    function updateActiveSuggestion(event){
-        const {activeSuggestion, filteredSuggestions} = state;
-        if (event.key === 'Enter'){
+    function updateActiveSuggestion(event) {
+        const { activeSuggestion, filteredSuggestions } = state;
+        if (event.key === 'Enter') {
             setState({
                 activeSuggestion: 0,
                 showSuggestions: false,
@@ -102,8 +146,8 @@ function Autocomplete(){
             });
         }
 
-        else if (event.key === 'ArrowUp'){
-            if (activeSuggestion === 0){
+        else if (event.key === 'ArrowUp') {
+            if (activeSuggestion === 0) {
                 return;
             }
             setState({
@@ -112,8 +156,8 @@ function Autocomplete(){
             });
         }
 
-        else if (event.key === 'ArrowDown'){
-            if (activeSuggestion + 1 === filteredSuggestions.length){
+        else if (event.key === 'ArrowDown') {
+            if (activeSuggestion + 1 === filteredSuggestions.length) {
                 return;
             }
             setState({
@@ -125,7 +169,7 @@ function Autocomplete(){
 
     return (
         <div>
-            <label htmlFor="autocomplete-city">City: </label>
+            {/* <label htmlFor="autocomplete-city">City: </label>
             <input 
                 id="autocomplete-city"
                 type="text" 
@@ -133,23 +177,58 @@ function Autocomplete(){
                 onClick={updateInput}
                 onKeyDown={updateActiveSuggestion}
                 // value={state.userInput}
+            /> */}
+            <TextInput
+                handleChange={updateUserInput}
+                inputVal={state.userInput}
             />
             <SuggestionsList state={state} />
         </div>
     );
 }
 
-function SuggestionsList({state}){
-    if (state.showSuggestions && state.userInput){
-        if (state.filteredSuggestions.length){
+function TextInput({ handleChange, inputVal }) {
+    // const [inputVal, setInputVal] = useState('');
+    // const [debouncedVal, setDebouncedVal] = useState('');
+
+    // function handleChange(event){
+    //     setInputVal(event.target.value);
+    // }
+
+    // Called whenever inputVal changes
+    // useEffect(() => {
+    //     const timeout = setTimeout(() => {
+    //         setDebouncedVal(inputVal);
+    //     }, 600);
+    //     return () => clearTimeout(timeout);
+    // }, [inputVal]);
+
+    // useEffect(() => console.log(debouncedVal), [debouncedVal]);
+
+    return (
+        <div>
+            <label htmlFor="autocomplete-city">City: </label>
+            <input
+                id="autocomplete-city"
+                type="text"
+                onChange={handleChange}
+                value={inputVal}
+            />
+        </div>
+    );
+}
+
+function SuggestionsList({ state }) {
+    if (state.showSuggestions && state.userInput) {
+        if (state.filteredSuggestions.length) {
             return (
                 <ul>
                     {state.filteredSuggestions.map((suggestion, index) => {
                         let liClass = 'li-inactive';
-                        if (index === state.activeSuggestion){
+                        if (index === state.activeSuggestion) {
                             liClass = 'li-active';
                         }
-                        return(
+                        return (
                             <li key={index} className={liClass}>{suggestion}</li>
                         );
                     })}
