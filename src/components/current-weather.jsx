1d2c5c3293aa4a88a6e99af83fb42066f2505dc7
guatilaM <guatilam@gmail.com>
2024-10-16 20:54:28 -0500

    feat: current weather - add and position svg icon as a component

--- src/components/current-weather.jsx
+++ src/components/current-weather.jsx
@@ -1,11 +1,13 @@
+import WeatherIcon from "./weather-icon";
+
 function CurrentWeather({main}) {
     return (
         <div className="current-weather">
             <h3 className="heading-3">Current weather</h3>
-            <div>
-                <div>general description</div>
-                <div>details</div>
+            <div id="cw-general">
+                <WeatherIcon />
             </div>
+            <div id="cw-details"></div>
         </div>
     );
 }
